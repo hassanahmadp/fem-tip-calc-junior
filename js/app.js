3fa437d3aa87414d9f562c6e4dc5850641011151
hassanahmadp <hassanahmadp@gmail.com>
2021-07-15 01:26:05 +0500

    going to sleep

--- js/app.js
+++ js/app.js
@@ -1,33 +1,32 @@
+const nop = document.querySelector("#nop");
+const cstmPerc = document.querySelector('#cstm-perc');
+const radios = document.querySelectorAll('input[type="radio"]');
 // number inputs on value of "0"
 (function () {
-    const numberInputs = document.querySelectorAll("#nop,#bill,#cstm-perc");
-    const nonDRed = document.querySelectorAll(".non-d-red");
+    const nonDRed = document.querySelector(".non-d-red");
 
-    document.addEventListener("keyup", (e) => {
+    document.addEventListener("keyup", e => {
         if (parseFloat(e.target.value) === 0) {
-            e.target.parentElement.parentElement.children[0].children[1].classList.add('active');
-            e.target.classList.add('active');
+            nop.classList.add('active');
+            nonDRed.classList.add('active');
         } else {
-            e.target.parentElement.parentElement.children[0].children[1].classList.remove('active');
-            e.target.classList.remove('active');
+            nop.classList.remove('active');
+            nonDRed.classList.remove('active');
         }
     });
 })();
 
 // all radios unchecked if custom is selected 
 (function () {
-    const cstmPerc = document.querySelector('#cstm-perc');
-    const radios = document.querySelectorAll('.radios');
 
     cstmPerc.addEventListener('focus', () => {
         for (let i = 0; i < radios.length; i++) {
-            radios[i].setAttribute('checked', false);
+            radios[i].checked = false;
         }
     });
 
 })()
 
-// // Actual functionality
-// (function () {
-
-// })();
+    // Actual functionality
+    (function () {
+    })();
